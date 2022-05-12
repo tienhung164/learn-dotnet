68eb05624d261eb2948071a50c2eb9f305a35a48
unknown <hungvt164@gmail.com>
2022-05-12 19:36:02 +0700

    day1

---
+++ day1/Account.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+    public class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Account(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public Account()
+        {
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($" username: {Username},password: {Password}");
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("nhap username");
+            Username = Console.ReadLine();
+            Console.WriteLine("nhap  passwprd");
+            Password = Console.ReadLine();
+        }
+        public bool Equal(Account input)
+        {
+            if (Password != input.Password || Username != input.Username)
+                return false;
+
+            return true;
+        }
+
+    }
+
+
+    public class Customer : Account
+    {
+        public string DOB { get; set; }
+        public string Name { get; set; }
+
+        public Customer(string username, string password, string dOB, string name) : base(username, password)
+        {
+
+
+            DOB = dOB;
+            Name = name;
+
+        }
+
+        public Customer()
+        {
+
+
+
+        }
+
+
+        public void ToString()
+        {
+            base.ToString();
+            Console.WriteLine($"dob: {DOB}, Name: {Name}");
+        }
+
+        public void Input()
+        {
+            base.Input();
+            Console.WriteLine("nhap DOB");
+            DOB = Console.ReadLine();
+            Console.WriteLine("nhap  Name");
+            Name = Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ day1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account("tienhung", "123");
+            Account account1 = new Account();
+            // account1.Input();
+            // Console.WriteLine(account.Equal(account1));
+
+
+            Customer customer = new Customer();
+            customer.Input();
+            customer.ToString();
+
+        }
+    }
+}
